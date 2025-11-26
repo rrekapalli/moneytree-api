2a5103eb051f09fd6e950eea3ddd64e4d78821b2
rrekapalli <rekapallichaitanya@gmail.com>
2025-11-26 23:45:35 +0530

    screener id issues fixed

--- backend/src/main/java/com/moneytree/screener/ScreenerSavedViewRepository.java
+++ backend/src/main/java/com/moneytree/screener/ScreenerSavedViewRepository.java
@@ -2,6 +2,7 @@ package com.moneytree.screener;
 
 import com.moneytree.screener.entity.ScreenerSavedView;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -11,10 +12,13 @@ import java.util.UUID;
 @Repository
 public interface ScreenerSavedViewRepository extends JpaRepository<ScreenerSavedView, UUID> {
 
+    @Query("SELECT ssv FROM ScreenerSavedView ssv WHERE ssv.screener.id = ?1")
     List<ScreenerSavedView> findByScreenerId(UUID screenerId);
 
+    @Query("SELECT ssv FROM ScreenerSavedView ssv WHERE ssv.user.id = ?1")
     List<ScreenerSavedView> findByUserId(UUID userId);
 
+    @Query("SELECT ssv FROM ScreenerSavedView ssv WHERE ssv.screener.id = ?1 AND ssv.user.id = ?2 AND ssv.name = ?3")
     Optional<ScreenerSavedView> findByScreenerIdAndUserIdAndName(UUID screenerId, UUID userId, String name);
 }
 
