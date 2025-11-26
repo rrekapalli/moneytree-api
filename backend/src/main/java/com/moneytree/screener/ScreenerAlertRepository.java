1b6b7a003eefa0267fae080241046ca3a3f62772
rrekapalli <rekapallichaitanya@gmail.com>
2025-11-26 23:44:19 +0530

    screen slert issue resolved

--- backend/src/main/java/com/moneytree/screener/ScreenerAlertRepository.java
+++ backend/src/main/java/com/moneytree/screener/ScreenerAlertRepository.java
@@ -2,6 +2,7 @@ package com.moneytree.screener;
 
 import com.moneytree.screener.entity.ScreenerAlert;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -10,6 +11,7 @@ import java.util.UUID;
 @Repository
 public interface ScreenerAlertRepository extends JpaRepository<ScreenerAlert, UUID> {
 
+    @Query("SELECT sa FROM ScreenerAlert sa WHERE sa.screener.id = ?1")
     List<ScreenerAlert> findByScreenerId(UUID screenerId);
 
     List<ScreenerAlert> findByIsEnabledTrue();
